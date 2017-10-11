2afc229c6b34d81f1501d3f6f4dbe05ae65600a5
Paul Trowbridge <fleetside72@hotmail.com>
2017-10-11 00:46:15 -0400

    backup files

--- ubm_backup.cmd
+++ ubm_backup.cmd
@@ -1,2 +1,2 @@
-C:\PostgreSQL\pg10\bin\pg_dump-h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "C:\users\fleet\Documnets\tps_etl\ubm_schema.backup"
-C:\PostgreSQL\pg10\bin\pg_dump-h localhost -p 5433 -U ptrowbridge -d ubm -a -O -F p -f "C:\users\fleet\Documnets\tps_etl\ubm_data.backup"
+"C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm -c -s -O -F p -f "C:\users\fleet\Documents\tps_etl\ubm_schema.backup"
+"C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm --column-inserts -a -O -F p -f "C:\users\fleet\Documents\tps_etl\ubm_data.backup"
