4e1cc79fb437479f29a69eba8d7d144c59381d55
Paul Trowbridge <fleetside72@hotmail.com>
2018-02-05 23:30:24 -0500

    rename and exclude data component

---
+++ deploy/dump.cmd
@@ -0,0 +1,1 @@
+"C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "C:\users\fleet\Documents\tps_etl\ubm_schema.sql"
--- deploy/rebuild_pg.cmd
+++
@@ -1,4 +0,0 @@
-"C:\PostgreSQL\pg10\bin\psql" -h localhost -p 5433 -d postgres -U postgres -c "DROP DATABASE ubm"
-"C:\PostgreSQL\pg10\bin\psql" -h localhost -p 5433 -d postgres -U postgres -c "CREATE DATABASE ubm"
-"C:\PostgreSQL\pg10\bin\psql" -h localhost -p 5433 -d ubm -U postgres -f "C:\users\fleet\documents\tps_etl\ubm_schema.sql"
-"C:\PostgreSQL\pg10\bin\psql" -h localhost -p 5433 -d ubm -U postgres -f "C:\users\fleet\documents\tps_etl\ubm_data.sql"
