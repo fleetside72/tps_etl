ea9b1ee5380817e04eb5f6852924463bd886c32d
Paul Trowbridge <fleetside72@hotmail.com>
2018-02-17 14:20:30 -0500

    merge c9 dump into master

--- deploy/dump.cmd
+++ deploy/dump.cmd
@@ -1,1 +1,5 @@
 "C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "C:\users\fleet\Documents\tps_etl\ubm_schema.sql"
+
+"/home/ubuntu/workspace/bigsql/pg10/bin/psql" -h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "/home/ubuntu/workspace/tps_etl/ubm_schema.sql"
+"/home/ubuntu/workspace/bigsql/pg10/bin/psql" -h localhost -p 5433 -U ptrowbridge -d ubm --column-inserts -a -O -F p -f "/home/ubuntu/workspace/tps_etl/ubm_data.sql"
+
