492e39372310b40f4a73c54377ff3f14db01a536
Paul Trowbridge <fleetside72@hotmail.com>
2018-02-23 14:52:02 -0500

    only dump tps schema

--- deploy/dump.cmd
+++ deploy/dump.cmd
@@ -1,5 +1,4 @@
-"C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "C:\users\fleet\Documents\tps_etl\ubm_schema.sql"
+"C:\PostgreSQL\pg10\bin\pg_dump" -h localhost -p 5433 -U ptrowbridge -d ubm -s -n "tps" -O -F p -f "C:\users\fleet\Documents\tps_etl\deploy\ubm_schema.sql"
 
-"/home/ubuntu/workspace/bigsql/pg10/bin/psql" -h localhost -p 5433 -U ptrowbridge -d ubm -s -O -F p -f "/home/ubuntu/workspace/tps_etl/ubm_schema.sql"
-"/home/ubuntu/workspace/bigsql/pg10/bin/psql" -h localhost -p 5433 -U ptrowbridge -d ubm --column-inserts -a -O -F p -f "/home/ubuntu/workspace/tps_etl/ubm_data.sql"
+"/home/ubuntu/workspace/bigsql/pg10/bin/psql" -h localhost -p 5433 -U ptrowbridge -d ubm -s -n "tps" -O -F p -f "/home/ubuntu/workspace/tps_etl/deploy/ubm_schema.sql"
 
