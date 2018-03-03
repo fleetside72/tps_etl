de2aac8d2397e73ab300525cb59b495e845e2a85
Paul Trowbridge <fleetside72@hotmail.com>
2018-03-03 11:52:34 -0500

    list out columns

--- reports/dcard_bal.sql
+++ reports/dcard_bal.sql
@@ -1,8 +1,12 @@
 \timing
 SELECT 
-    r.*,
-    rec->'id',
-    SUM(r."Amount") OVER (PARTITION BY srce ORDER BY r."Post Date" asc , rec->>'id' asc, r."Description") + 1061.1 + 22.40
+    r."Trans. Date",
+    r."Post Date",
+    r."Description",
+    r."Amount",
+    r."Category",
+    rec->'id' id,
+    SUM(r."Amount") OVER (PARTITION BY srce ORDER BY r."Post Date" asc , rec->>'id' asc, r."Description") + 1061.1 + 22.40 balance
 FROM 
     tps.trans
     LEFT JOIN LATERAL jsonb_populate_record(null::tps.dcard, rec) r ON TRUE
