0196ba4c532916f505cc8ca5937ef4b237b5a28c
Paul Trowbridge <ptrowbridge@hc-companies.com>
2018-05-25 13:44:37 -0400

    drop old files and start working on json storage scripts

--- sample_google_api/test_scipt.sql
+++ sample_google_api/test_scipt.sql
@@ -5,24 +5,38 @@ SELECT
     jsonb_pretty(r.x) 
 FROM
     tps.srce_set(
-    'DMAPI',
     $$
-    {
-        "name": "DMAPI",
-        "type": "csv",
-        "schema": [
+{
+    "name": "DMAPI",
+    "type": "csv",
+    "schemas": {
+        "default": [
             {
-                "key": "doc",
-                "type": "jsonb"
+                "path": "{doc,origin_addresses,0}",
+                "type": "text",
+                "column_name": "origin_address"
+            },
+            {
+                "path": "{doc,destination_addresses,0}",
+                "type": "text",
+                "column_name": "destination_address"
+            },
+            {
+                "path": "{doc,rows,0,elements,0,distance,value}",
+                "type": "numeric",
+                "column_name": "distince"
+            },
+            {
+                "path": "{doc,rows,0,elements,0,duration,value}",
+                "type": "numeric",
+                "column_name": "duration"
             }
         ],
-        "unique_constraint": {
-            "type": "key",
-            "fields": [
-            	"{doc}"
-            ]
-        }
+        "constraint": [
+            "{doc}"
+        ]
     }
+}
     $$
 ) r(x);
 --------------------------build a csv file---------------------
