3abb2fa9babb9938828a10da61f7edb3bf41006f
Paul Trowbridge <ptrowbridge@hc-companies.com>
2018-05-30 15:10:46 -0400

    load paycom

--- reload/paycom/extract.sql
+++ reload/paycom/extract.sql
@@ -2,31 +2,57 @@
 COPY
 (
 SELECT
-	to_char(r."AsOfDate",'mm/dd/yyyy') "AsOfDate"
-	,r."BankId"
-	,r."AccountNumber"
-	,r."AccountName"
-	,r."BaiControl"
-	,r."Currency"
-	,r."Transaction"
-	,r."Reference"
-	,r."Amount"
-	,r."Description"
+	r."perd_start",
+	r."perd_end",
+	r."check_date",
+	r."loc_code",
+	r."loc_descr",
+	r."loc_glseg",
+	r."loc_over",
+	r."dep_code",
+	r."dep_descr",
+	r."dep_nat",
+	r."dep_over",
+	r."di_code",
+	r."di_descr",
+	r."di_glseg",
+	r."di_over",
+	r."title_code",
+	r."title_descr",
+	r."title_glseg",
+	r."title_over",
+	r."ee_code",
+	r."ee_glseg",
+	r."ee_over",
+	r."acct_type_code",
+	r."hours",
+	r."nat_code",
+	r."nat_over",
+	r."gl_ref",
+	r."gl_group",
+	r."gl_descr",
+	r."gl_code",
+	r."gl_amount",
+	r."pp_code",
+	r."pp_descr",
+	r."pp_gl",
+	r."pp_over",
+	r."transaction"
 FROM
 	tps.trans
-	JOIN LATERAL jsonb_populate_record(NULL::tps.pncc, rec) r ON TRUE
+	JOIN LATERAL jsonb_populate_record(NULL::tps.PAYCOM, rec) r ON TRUE
 WHERE
-	srce = 'PNCC'
+	srce = 'PAYCOM'
 )
-TO 'C:\users\ptrowbridge\downloads\pncc.csv' WITH (format csv, header TRUE)
+TO 'C:\users\ptrowbridge\downloads\PAYCOM.csv' WITH (format csv, header TRUE)
 
 --source
-SELECT DEFN FROM TPS.SRCE WHERE SRCE = 'PNCC'
+SELECT DEFN FROM TPS.SRCE WHERE SRCE = 'PAYCOM'
 
 --mapdef
-SELECT jsonb_agg(row_to_json(x)::jsonb) FROM (SELECT srce, target "name", regex, seq "sequence" FROM tps.map_rm WHERE srce = 'PNCC') x
+SELECT jsonb_agg(row_to_json(x)::jsonb) FROM (SELECT srce, target "name", regex, seq "sequence" FROM tps.map_rm WHERE srce = 'PAYCOM') x
 
 --map values
-SELECT jsonb_agg(row_to_JSON(x)::jsonb) FROM (SELECT srce "source", target "map", retval ret_val, "map" mapped FROM tps.map_rv WHERE srce = 'PNCC') X
+SELECT jsonb_agg(row_to_JSON(x)::jsonb) FROM (SELECT srce "source", target "map", retval ret_val, "map" mapped FROM tps.map_rv WHERE srce = 'PAYCOM') X
 
 
