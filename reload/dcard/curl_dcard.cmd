501654bd8dbe048c9c4247e3e560e9ba585f9a10
Paul Trowbridge <fleetside72@hotmail.com>
2018-05-30 22:42:06 -0400

    consolidate curls to one file

--- reload/dcard/curl_dcard.cmd
+++ reload/dcard/curl_dcard.cmd
@@ -1,1 +1,4 @@
+curl -H "Content-Type: application/json" -X POST -d@./srce.json http://localhost:81/srce_set
+curl -H "Content-Type: application/json" -X POST -d@./mapdef.json http://localhost:81/mapdef_set
+curl -H "Content-Type: application/json" -X POST -d@//mnt/c/Users/fleet/Documents/tps_etl/reload/mapval.json http://localhost:81/mapval_set
 curl -v -F upload=@//mnt/c/Users/fleet/Downloads/DFS-Search-20180529.csv http://localhost:81/import?srce=dcard
