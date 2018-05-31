e20ffafe513d5068b981d44ae75e562988143392
Paul Trowbridge <fleetside72@hotmail.com>
2018-05-31 00:28:04 -0400

    update files names in curl

--- deploy/reload/dcard/curl_dcard.cmd
+++ deploy/reload/dcard/curl_dcard.cmd
@@ -1,4 +1,4 @@
 curl -H "Content-Type: application/json" -X POST -d@./srce.json http://localhost:81/srce_set
-curl -H "Content-Type: application/json" -X POST -d@./mapdef.json http://localhost:81/mapdef_set
-curl -H "Content-Type: application/json" -X POST -d@./mapval.json http://localhost:81/mapval_set
-curl -v -F upload=@//mnt/c/Users/fleet/Downloads/DFS-Search-20180529.csv http://localhost:81/import?srce=dcard
+curl -H "Content-Type: application/json" -X POST -d@./map.json http://localhost:81/mapdef_set
+curl -H "Content-Type: application/json" -X POST -d@./vals.json http://localhost:81/mapval_set
+curl -v -F upload=@//mnt/c/Users/fleet/Downloads/dcard.csv http://localhost:81/import?srce=dcard
