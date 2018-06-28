355a1a15fa1f09a8f15952d66eeef73b9f7e6a57
Paul Trowbridge <fleetside72@hotmail.com>
2018-06-28 01:01:02 -0400

    change to match up with api

--- database/deploy/reload/dcard/curl_dcard.cmd
+++ database/deploy/reload/dcard/curl_dcard.cmd
@@ -1,4 +1,4 @@
-curl -H "Content-Type: application/json" -X POST -d@./srce.json http://localhost/srce_set
-curl -H "Content-Type: application/json" -X POST -d@./map.json http://localhost/mapdef_set
-curl -H "Content-Type: application/json" -X POST -d@./vals.json http://localhost/mapval_set
+curl -H "Content-Type: application/json" -X POST -d@./srce.json http://localhost/source
+curl -H "Content-Type: application/json" -X POST -d@./map.json http://localhost/regex
+curl -H "Content-Type: application/json" -X POST -d@./vals.json http://localhost/mapping
 curl -v -F upload=@./d.csv http://localhost/import?srce=dcard
